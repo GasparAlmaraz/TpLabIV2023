2266dd6fe5b59c089b27d7b9646b24a2cd8d4815
GasparAlmaraz <gasparalmaraz.2013@gmail.com>
2023-10-30 23:35:59 -0300

    (WIP): Defined body for detail component, wip logic of detail

--- src/app/components/pokekon-detail/pokemon-detail.component.ts
+++ src/app/components/pokekon-detail/pokemon-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Pokemon } from 'src/app/models/pokemon';
 import { PokemonService } from 'src/app/services/pokemon.service';
 
@@ -9,13 +9,14 @@ import { PokemonService } from 'src/app/services/pokemon.service';
 })
 export class PokemonDetailComponent {
 
+  @Input()
   pokemon : Pokemon | undefined;
+  
   isLoaded = false;
 
   constructor(private pokemonService: PokemonService){}
 
   ngOnInit(){
-    this.pokemon = new Pokemon();
   }
   
   loadInformation() {
