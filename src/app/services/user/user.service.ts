61a344b4c122d36bdfc95270e0784c6f4aa192c8
GasparAlmaraz <gasparalmaraz.2013@gmail.com>
2023-11-09 19:46:11 -0300

    (WIP): Deleted JSON logic on services, now implementing API for that

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 
@@ -10,72 +11,26 @@ export class UserService {
 
   currentUser: User | undefined;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  generateUserFile(createdUser: User, callback: (success: boolean, message: string) => void) {
+  generateUserFile(createdUser: User) {
     let newUser = new User();
     newUser.username = createdUser.username;
     newUser.password = createdUser.password;
     newUser.wallet = 0;
     newUser.ownedPokemonIds = new Array<number>();
-  
-    const filePath = `../../data/${newUser.username}.json`;
+    
+    let jsonData = JSON.stringify(newUser);
+    const result = this.http.post(`http://localhost:3001/postUser`, jsonData);
 
-    fs.readFile(filePath)
-    .then(() => {
-      callback(false, `This UserName already exists, try again.`);
-    })
-    .catch(() => {
-      let dataToSave = JSON.stringify(newUser);
-      fs.writeFile(filePath, dataToSave)
-        .then(() => {
-          console.log("Success creating file.");
-          this.currentUser = newUser;
-          callback(true, `UserData created, FileName: ${newUser.username}`);
-        })
-        .catch((error: any) => {
-          console.log(error);
-          callback(false, `Something wrong occurred: ${error}`);
-        });
-    })
+    return result;
   }
 
-  updateUserFile(userToUpdate: User, callback: (success: boolean, message: string) => void){
-    const filePath = `../../data/${userToUpdate.username}.json`;
-
-    fs.readFile(filePath)
-    .then(() => {
-      let dataToUpdate = JSON.stringify(userToUpdate);
-
-      fs.writeFile(filePath, dataToUpdate)
-        .then(() => {
-          console.log("Success updating file.");
-          this.currentUser = userToUpdate;
-          callback(true, `UserData updated.`);
-        })
-        .catch((error: any) => {
-          console.log(error);
-          callback(false, `Something wrong occurred: ${error}`);
-        });
-    })
-    .catch((error: any) => {
-      console.log(error);
-      callback(false, `The file does not exist: ${error}`);
-    })
+  updateUserFile(userToUpdate: User){
+    return this.http.put(`http://localhost:3001/updateUser`, userToUpdate);
   }
 
-  login(userLogin: User, callback: (success: boolean, message: string) => void){
-    const filePath = `../../data/${userLogin.username}.json`;
-
-    fs.readFile(filePath)
-    .then(() => {
-      console.log("Success login in.");
-      this.currentUser = userLogin;
-      callback(true, `Welcome ${userLogin.username}.`);
-    })
-    .catch((error: any) => {
-      console.log(error);
-      callback(false, `This user does not exist. Try register first.`);
-    })
+  login(userLogin: User){
+    
   }
 }
